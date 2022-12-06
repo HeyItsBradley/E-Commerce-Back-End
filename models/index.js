5571244ac2cac44396b68d9b1c6cfefe59190c91
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-12-05 19:16:28 -0700

    completed associations in the index for models

--- models/index.js
+++ models/index.js
@@ -5,16 +5,16 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
+Product.belongsTo(Category);
+// Categories have many Products
+Category.hasMany(Product, {
   foreignKey: "id",
   onDelete: "CASCADE",
 });
-// Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag });
 module.exports = {
   Product,
   Category,
