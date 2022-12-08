0792e257123b7a6c51627cb4d6b584dd3c89568e
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-12-07 21:03:40 -0700

    added delete route to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,6 +68,11 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  /* req.body should look like this
+  {
+    category_name: "Graphics card"
+  }
+  */
   try {
     const categoryData = await Category.update(req.body, {
       where: {
